f27e8f2729a09599ad2452b6c56276ea105a26b4
Jabrhm <jabrhmmf998@gmail.com>
2025-12-31 01:13:00 +0000

    Create home.nix

---
+++ nixConf/home.nix
@@ -0,0 +1,51 @@
+{ config, pkgs, inputs, lib, ... }:
+let
+  tex = (pkgs.texliveBasic.withPackages (
+    ps: with ps; [
+      scheme-full 
+      pspicture
+    ]));
+in
+{
+  home.username = "abraham";
+  home.homeDirectory = "/home/abraham";
+
+  home.stateVersion = "25.11"; 
+
+  home.packages = [
+    # Cute things
+    pkgs.cava
+    pkgs.cmatrix
+    pkgs.htop-vim
+    pkgs.kittysay
+    pkgs.lsd 
+    pkgs.ranger 
+    pkgs.sl 
+    pkgs.tty-clock 
+
+    # College 
+    pkgs.wxhexeditor
+    pkgs.zathura
+    tex 
+  ];
+
+  home.file = {
+  };
+
+  home.sessionVariables = {
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      btw = "echo I use nix, btw";
+      nix-rebuild = "sudo nixos-rebuild switch --flake ~/nixConf";
+      vi = "emacs -nw";
+      vim = "emacs -nw";
+      nvim = "emacs -nw";
+    };
+  };
+}
