54f674719b1791c18b0a3f0f6bcdf6f87361b4f3
Jabrhm <jabrhmmf998@gmail.com>
2026-01-12 15:07:52 -0600

    THings

--- nixConf/flake.nix
+++ nixConf/flake.nix
@@ -3,9 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    battery.url = "path:./Modules/Battery";
-    miku-grub.url = "path:./Modules/Miku-Grub";
-    
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,36 +14,50 @@
   outputs =
    inputs@{
      nixpkgs,
+     nixpkgs-unstable,
      home-manager,
-     battery,
-     miku-grub,
      ...
-    }: {
-    nixosConfigurations = {
-      dematoso = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          inherit inputs;
-        };
-        
-        modules = [
-          ./configuration.nix
-          battery.nixosModules.default
-          miku-grub.nixosModules.default
-          
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs= true;
-              useUserPackages = true;
-              users."abraham" = ./home.nix;
-              backupFileExtension = "backup";
-            };
-          }
-          
-        ];
-        
-      };
-    };
-  };
+   }:
+   let
+     system = "x86_64-linux";
+
+     overlay-unstable = final: prev: {
+       unstable = import nixpkgs-unstable {
+         system = prev.stdenv.hostPlatform.system;
+         config.allowUnfree = true;
+       };
+     };
+   in
+   {
+     nixosConfigurations = {
+       dematoso = nixpkgs.lib.nixosSystem {
+         inherit system;
+         specialArgs = {
+           inherit inputs;
+         };
+         
+         modules = [
+           ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
+           ./configuration.nix
+           
+           home-manager.nixosModules.home-manager
+           {
+             home-manager = {
+               useGlobalPkgs= true;
+               useUserPackages = true;
+               users."abraham" = ./home.nix;
+               backupFileExtension = "backup";
+             };
+           }
+         ];
+       };
+     };
+
+     homeConfigurations."abraham" = home-manager.lib.homeManagerConfiguration {
+       modules = [
+         ./home.nix
+       ];
+       extraSpecialArgs = { inherit inputs; };
+     };
+   };
 }
