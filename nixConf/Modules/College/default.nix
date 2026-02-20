166373fec5b39e0ccfa6845eb31475c3d3727e99
Jabrhm <jabrhm@protonmail.com>
2026-02-19 21:36:35 -0600

    Create default.nix

---
+++ nixConf/Modules/College/default.nix
@@ -0,0 +1,41 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+
+{
+  config = {
+    # My hp printer\scanner 
+    services.printing.enable = true;
+    services.printing.drivers = [ pkgs.hplip ];
+    
+    hardware.sane.enable = true;
+    hardware.sane.extraBackends = [ pkgs.hplipWithPlugin ]; 
+    
+    # Programs
+    programs = {
+      thunderbird = {
+        enable = true;
+        package = pkgs.unstable.thunderbird;
+      };
+      kdeconnect.enable = true;
+      
+      # Fuck nano
+      nano.enable = false;
+    };
+    
+    # LSP thing
+    nix.nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
+
+    environment.systemPackages = with pkgs; [
+      # LSPs
+      pkgs.unstable.clang-tools 
+      pkgs.unstable.texlab 
+      pkgs.unstable.nixd
+
+      # DAPs 
+    ];
+  };
+}
